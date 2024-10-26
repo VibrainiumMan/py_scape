95191a022e15ab585d35ba5fbf2c4e65d2191a6a
Adrian Chand <adidasiscool12345@gmail.com>
2024-10-27 10:17:05 +1300

    simple scraper

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+import requests
+from bs4 import BeautifulSoup
+
+request = requests.get('https://www.seek.co.nz/?tracking=CLP-WEB-SeekLandingPage')
+
+print(request)
+
+parser = BeautifulSoup(request.content, 'html.parser')
+print(parser.prettify())
